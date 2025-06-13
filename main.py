64e18f6aff3584a63533bf6d21f1843353f11327
AverageJoe6359 <homer.joseph69@gmail.com>
2025-06-12 21:42:45 -0400

    B: player created

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame # type: ignore
 from constants import *
+from player import Player
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}") 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    fps_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = (fps_clock.tick(60) / 1000)
+        
 if __name__ == "__main__":
     main()
